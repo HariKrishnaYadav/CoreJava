bee3aca445bb60652f393aa254500f4eaacebd33
“HariKrishnaYadav” <harekrishna.bhuit15@gmail.com>
2024-04-09 15:58:56 +0530

    first commit for basic program

---
+++ src/com/hky/designPattern/factory/FourWheeler.java
@@ -0,0 +1,8 @@
+package com.hky.designPattern.factory;
+
+class FourWheeler extends Vehicle { 
+    public void printVehicleInfo() 
+    { 
+        System.out.println("I am four wheeler"); 
+    } 
+} 
